de766b62de9b48f4164501df310fabefef757dcb
Hugo Ochoa <hugo.ochoa.h@gmail.com>
2021-04-21 17:04:51 -0500

    update: add style updates - Update favicon, logo and background image

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,75 +1,70 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title>Lista de Regalos de Fer y Luiso</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <span v-if="id"
-        ><v-icon left>{{ icons.mdiAccount }}</v-icon
-        >{{ id }}</span
-      >
-    </v-app-bar>
+  <div id="app">
+    <span class="bg"></span>
+    <v-app id="inspire">
+      <v-app-bar color="white" app>
+        <img class="mr-3" src="/logo.png" height="60"/>
+        <v-spacer></v-spacer>
+        <span v-if="id"
+          ><v-icon left>{{ icons.mdiAccount }}</v-icon
+          >{{ id }}</span
+        >
+      </v-app-bar>
 
-    <v-main class="grey lighten-1">
-      <v-container>
-        <v-row>
-          <template v-for="category in categories">
-            <v-col :key="category" class="mt-2" cols="12">
-              <strong>{{ category }}</strong>
-            </v-col>
+      <v-main>
+        <v-container>
+          <v-row>
+            <template v-for="category in categories">
+              <v-col :key="category" class="mt-2" cols="12">
+                <strong>{{ category }}</strong>
+              </v-col>
 
-            <v-col
-              v-for="gift in getCategoryGifts(category)"
-              :key="gift.name"
-              cols="12"
-              md="4"
-            >
-              <GiftCard
-                :gift="gift"
-                :id="id"
-                @select="onGiftSelect(gift)"
-                @cancel="onGiftCancel(gift)"
-              ></GiftCard>
-            </v-col>
+              <v-col
+                v-for="gift in getCategoryGifts(category)"
+                :key="gift.name"
+                cols="12"
+                md="4"
+              >
+                <GiftCard
+                  :gift="gift"
+                  :id="id"
+                  @select="onGiftSelect(gift)"
+                  @cancel="onGiftCancel(gift)"
+                ></GiftCard>
+              </v-col>
+            </template>
+          </v-row>
+        </v-container>
+
+        <InvitationDialog
+          :dialog="invitationDialog"
+          @accept="onInvitationAccept"
+        ></InvitationDialog>
+
+        <WelcomeDialog
+          :dialog="welcomeDialog"
+          @accept="onWelcomeAccept"
+        ></WelcomeDialog>
+
+        <ConfirmationDialog
+          :dialog="confirmationDialog"
+          :mode="confirmationDialogMode"
+          :gift="selectedGift"
+          @accept="onConfirmationAccept"
+          @cancel="onConfirmationCancel"
+        ></ConfirmationDialog>
+
+        <v-snackbar v-model="snackbar">
+          Cambio guardado con éxito
+          <template v-slot:action="{ attrs }">
+            <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+              Cerrar
+            </v-btn>
           </template>
-        </v-row>
-      </v-container>
-
-      <InvitationDialog
-        :dialog="invitationDialog"
-        @accept="onInvitationAccept"
-      ></InvitationDialog>
-
-      <WelcomeDialog
-        :dialog="welcomeDialog"
-        @accept="onWelcomeAccept"
-      ></WelcomeDialog>
-
-      <ConfirmationDialog
-        :dialog="confirmationDialog"
-        :mode="confirmationDialogMode"
-        :gift="selectedGift"
-        @accept="onConfirmationAccept"
-        @cancel="onConfirmationCancel"
-      ></ConfirmationDialog>
-
-      <v-snackbar
-        v-model="snackbar"
-      >
-        Cambio guardado con éxito
-        <template v-slot:action="{ attrs }">
-          <v-btn
-            color="pink"
-            text
-            v-bind="attrs"
-            @click="snackbar = false"
-          >
-            Close
-          </v-btn>
-        </template>
-      </v-snackbar>
-
-    </v-main>
-  </v-app>
+        </v-snackbar>
+      </v-main>
+    </v-app>
+  </div>
 </template>
 
 <script>
@@ -84,7 +79,7 @@ const API_BASE_URL = "http://3.143.179.41:3000";
 
 export default {
   mounted() {
-    this.fetch()
+    this.fetch();
   },
   data: () => ({
     id: "",
@@ -97,7 +92,7 @@ export default {
       mdiAccount,
     },
     gifts: [],
-    snackbar: false
+    snackbar: false,
   }),
   computed: {
     categories() {
@@ -143,17 +138,17 @@ export default {
     },
     onConfirmationAccept(gift) {
       this.confirmationDialog = false;
-      const params = { reserved_by: this.confirmationDialogMode === 'select' ? this.id : '' }
-      
-      axios
-        .patch(`${API_BASE_URL}/gifts/${gift._id}`, params)
-        .then(() => {
-          this.fetch()
-          this.snackbar = true
-          setTimeout(() => {
-            this.snackbar = false
-          }, 5000)
-        });
+      const params = {
+        reserved_by: this.confirmationDialogMode === "select" ? this.id : "",
+      };
+
+      axios.patch(`${API_BASE_URL}/gifts/${gift._id}`, params).then(() => {
+        this.fetch();
+        this.snackbar = true;
+        setTimeout(() => {
+          this.snackbar = false;
+        }, 5000);
+      });
     },
     onConfirmationCancel() {
       this.confirmationDialog = false;
@@ -161,3 +156,18 @@ export default {
   },
 };
 </script>
+
+<style>
+.bg {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url("/background.png")
+    no-repeat center center;
+  background-size: cover;
+  background-color: red;
+  transform: scale(1.1);
+}
+</style>
